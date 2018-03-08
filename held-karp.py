cc9aac16f6e4b1a06ef277fbd28738c6b31697e1
Shaun <shjsawyer@gmail.com>
2018-03-08 10:01:53 -0500

    heldkarp: begin recursive implementation

--- held-karp.py
+++ held-karp.py
@@ -54,6 +54,57 @@ def held_karp(distance_matrix):
     return f(0, 0, [])
 
 
+def held_karp_dp(distance_matrix):
+    '''
+    Above algorithm, but making use of memoization to avoid recomputing
+    overlapping subproblems
+    '''
+    d = distance_matrix
+    n = len(d)
+
+    '''
+    We need a dp table that will store the minimum distances from city i
+    to city 0 that passes through all unvisitied cities in the bit mask.
+    There are n cities, and 2^n possible binary strings of length n, so our
+    table will have dimensions n x 2^n
+    '''
+    dp = [[None for i in xrange(2**n)] for j in xrange(n)]
+
+    def f(i, visited, path_so_far):
+        '''
+        f is defined as in the purely recursive implementation above.
+        The only difference here is that we check if the value we are
+        looking for is already in the defined dp table.
+
+        To retrive the path, we also need to keep track of the parent node
+        for any given i and remaining unvisited cities in the mask.
+        '''
+        # Check the table
+        if dp[i][visited]:
+            return dp[i][visited]
+        # Base case: check if all cities have been visited
+        if visited == (1 << n) - 1:
+            # we have visited all cities, return to 0
+            return d[i][0], path_so_far + [0,]
+
+        min_dist = sys.maxint
+        # visit all unvisited cities
+        for j in range(n):
+            if not (1 << j) & visited:
+                dist_from_j, path_with_j = \
+                    f(j, visited | (1 << j), path_so_far + [j,])
+                # Distance with j
+                dist_with_j = d[i][j] + dist_from_j
+                if dist_with_j < min_dist:
+                    min_dist = dist_with_j
+                    min_path = path_with_j
+
+        return min_dist, min_path
+
+    return f(0, 0, [])
+
+
+
 class Vertex:
     def __init__(self, x, y):
         self.x = float(x)
