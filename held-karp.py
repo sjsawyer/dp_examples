97f0890a81e5d4cc08c10fa51592020401a457bd
Shaun <shjsawyer@gmail.com>
2018-03-16 09:47:50 -0400

    clean up

--- held-karp.py
+++ held-karp.py
@@ -1,12 +1,16 @@
 '''
 held-karp.py
+
 Implementation of the Bellman-Held-Karp Algorithm to exactly solve TSPs,
 requiring no external dependencies.
+
+Includes a purely recursive implementation, as well as both top-down and
+bottom-up dynamic programming approaches.
 '''
 import sys
 
 
-def held_karp(distance_matrix):
+def held_karp_recursive(distance_matrix):
     '''
     Solution to TSP using the Bellman-Held-Karp Algorithm
 
@@ -19,8 +23,8 @@ def held_karp(distance_matrix):
 
     def f(i, visited, path_so_far):
         '''
-        Let f(i, visited, path_so_far) be the path of minimum distance from city i to
-        city 0, that passes through all remaining unvisited cities in
+        Let f(i, visited, path_so_far) be the path of minimum distance from
+        city i to city 0, that passes through all remaining unvisited cities in
         `visited`, where visited is a bitmask such that the bit in the jth
         position being 1 represents city j having been visited, and bit j
         being 0 represents city j having not been visited, and `path_so_far` is
@@ -55,7 +59,7 @@ def held_karp(distance_matrix):
     return f(0, 0, [])
 
 
-def held_karp_dp(distance_matrix):
+def held_karp_topdown(distance_matrix):
     '''
     Above algorithm, but making use of memoization to avoid recomputing
     overlapping subproblems
@@ -71,8 +75,7 @@ def held_karp_dp(distance_matrix):
 
     With this approach, we use another table called 'child' that keeps track
     of the child city of i for each combination of (i, visited), and we can
-    use this table to backtrack through our solution to obtain the actual
-    hamiltonian cycle of minimum distance.
+    use this table to obtain the actual Hamiltonian cycle of minimum distance.
     '''
     dp = [[None for i in xrange(2**n)] for j in xrange(n)]
     child = [[None for i in xrange(2**n)] for j in xrange(n)]
@@ -128,15 +131,15 @@ def held_karp_dp(distance_matrix):
     return ans, path
 
 
-def held_karp_dp_bottomup(distance_matrix):
+def held_karp_bottomup(distance_matrix):
     '''
     In the bottom up implementation, we compute all possible solutions for the
     values `i` and `visited` as in the implementations above, and then
     simply look up the value for f(0,0).
-    With this approach, we use another table called 'child' that keeps track
-    of the child city of i for each combination of (i, visited), and we can
-    use this table to backtrack through our solution to obtain the actual
-    hamiltonian cycle of minimum distance.
+
+    With this approach, we use the dp table, the original `distance_matrix`
+    and knowledge of the optimal cost to work backwards in determing what
+    the optimal path was.
     '''
     d = distance_matrix
     n = len(d)
@@ -145,7 +148,7 @@ def held_karp_dp_bottomup(distance_matrix):
 
     # Base case:
     # Distance from any city i back to 0 after having visited all cities
-    for i in range(n):
+    for i in xrange(n):
         dp[i][(1<<n)-1] = d[i][0]
 
     # Fill in all values of the dp table, excluding the values from the
@@ -170,7 +173,7 @@ def held_karp_dp_bottomup(distance_matrix):
     i, visited = 0, 1
     cost_from_i = dp[i][visited]
     while visited != (1 << n)-1:
-        for j in range(n):
+        for j in xrange(n):
             if not visited & (1 << j):
                 cost_from_j = dp[j][visited | (1 << j)]
                 # require a tolerance for real valued distances
@@ -178,7 +181,7 @@ def held_karp_dp_bottomup(distance_matrix):
                     # j was the city selected in the opt solution
                     path.append(j)
                     i, visited = j, visited | (1 << j)
-                    cost_from_i = cost_from_j 
+                    cost_from_i = cost_from_j
                     break
     # We have visited all cities, so return to 0
     path.append(0)
@@ -187,41 +190,48 @@ def held_karp_dp_bottomup(distance_matrix):
 
 
 class Vertex:
+    ''' Simple implementation of a point in Euclidean space '''
     def __init__(self, x, y):
         self.x = float(x)
         self.y = float(y)
 
 
 def distance(v1, v2):
-    ''' distance between two `Vertex` instances '''
+    ''' Euclidean distance between two `Vertex` instances '''
     return ((v1.x - v2.x)**2 + (v1.y - v2.y)**2)**0.5
 
 
-def adj_matrix(graph):
-    d = [[None for v in graph] for v in graph]
-    for i in xrange(len(d)):
-        for j in xrange(len(d[i])):
-            d[i][j] = distance(graph[i], graph[j])
-    return d
+def adjacency_matrix(graph):
+    '''
+    Construct the corresponding adjacency matrix from a list of verticies in a
+    graph, assumed to be a complete graph.
+    '''
+    m = [[None for v in graph] for v in graph]
+    for i in xrange(len(m)):
+        for j in xrange(len(m[i])):
+            m[i][j] = distance(graph[i], graph[j])
+    return m
 
 
 def main():
+
+    ## Test cases
+
     # g1: (16.0, [0, 2, 1, 3, 0])
+    g1 = [Vertex(0, 0), Vertex(4, 4), Vertex(4, 0), Vertex(0, 4)]
+    m1 = adjacency_matrix(g1)
+    for solver in held_karp_recursive, held_karp_topdown, held_karp_bottomup:
+        cost, path = solver(m1)
+        assert cost == 16.0
+        assert path == [0, 2, 1, 3, 0]
+
     # g2: (15.773387165490545, [0, 3, 1, 2, 4, 0])
-    #g1 = [Vertex(0, 0), Vertex(4, 4), Vertex(4, 0), Vertex(0, 4)]
     g2 = [Vertex(0, 0), Vertex(4, 4), Vertex(0, 3), Vertex(4, 0), Vertex(1, 2)]
-    #sol1= held_karp(adj_matrix(g1))
-    #sol2= held_karp(adj_matrix(g2))
-    #sol1 = held_karp_dp(adj_matrix(g1))
-    #sol2 = held_karp_dp(adj_matrix(g2))
-    sol2 = held_karp_dp_bottomup(adj_matrix(g2))
-
-    #print sol1
-    print sol2
-
-    #assert(sol1[0]==16.0)
-    assert(abs(sol2[0]-15.7733871)<0.001)
-
+    m2 = adjacency_matrix(g2)
+    for solver in held_karp_recursive, held_karp_topdown, held_karp_bottomup:
+        cost, path = solver(m2)
+        assert abs(cost - 15.7733871) < 0.001
+        assert path == [0, 3, 1, 2, 4, 0]
 
 
 if __name__ == '__main__':
